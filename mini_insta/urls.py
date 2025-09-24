24e10fdc908880e3ea3202d59a65ada8d0ad4ba9
wfugate <wfugate@bu.edu>
2025-09-24 15:28:39 -0400

    added show all profiles page to mini_insta

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,3 +1,6 @@
+from django.urls import path
+from .views import ProfileListView
 urlpatterns = [
+    path('', ProfileListView.as_view(), name='show_all_profiles'),
     
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Profile
 # Create your views here.
+
+
+class ProfileListView(ListView):
+    model = Profile
+    template_name = 'mini_insta/show_all_profiles.html'
+    context_object_name = 'profiles'
