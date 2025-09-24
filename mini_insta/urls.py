a38aedd85a4d49a7406b0ea44a1b5cc3a199fadf
wfugate <wfugate@bu.edu>
2025-09-24 15:37:27 -0400

    finished structure and logic of mini_insta

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import ProfileListView
+from .views import ProfileDetailView
 urlpatterns = [
     path('', ProfileListView.as_view(), name='show_all_profiles'),
+    path('profile/<int:pk>/', ProfileDetailView.as_view(), name='show_profile'),
     
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Profile
+from django.views.generic import DetailView
 # Create your views here.
 
 
@@ -8,3 +9,8 @@ class ProfileListView(ListView):
     model = Profile
     template_name = 'mini_insta/show_all_profiles.html'
     context_object_name = 'profiles'
+
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = 'mini_insta/show_profile.html'
+    context_object_name = 'profile'
