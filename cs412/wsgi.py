f5651ad2050cb72d75ac9463091a1920fe38304b
wfugate <wfugate@bu.edu>
2025-12-01 01:11:20 -0500

    fixed wsgi.py

--- cs412/wsgi.py
+++ cs412/wsgi.py
@@ -1,13 +1,15 @@
 """
 WSGI config for cs412 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import site
+
+# Add user site-packages to path (for server deployment)
+user_site = os.path.expanduser('~/.local/lib/python3.12/site-packages')
+if os.path.exists(user_site):
+    site.addsitedir(user_site)
 
 from django.core.wsgi import get_wsgi_application
 
